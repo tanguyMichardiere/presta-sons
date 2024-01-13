8507055e77dac2dda3c2395b64fd550580c40baa
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-01-13 13:04:48 +0100

    add tests

---
+++ src/utils/embed/tag.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "vitest";
+import { idFromTag, tagFromId } from "./tag";
+
+describe("idFromTag", function () {
+  test("simple", function () {
+    expect(idFromTag("<@id>")).toBe("id");
+  });
+
+  test("long", function () {
+    expect(idFromTag("<@veryLongId>")).toBe("veryLongId");
+  });
+});
+
+describe("tagFromId", function () {
+  test("simple", function () {
+    expect(tagFromId("id")).toBe("<@id>");
+  });
+
+  test("long", function () {
+    expect(tagFromId("veryLongId")).toBe("<@veryLongId>");
+  });
+});
