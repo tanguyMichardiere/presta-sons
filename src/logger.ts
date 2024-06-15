2bef3e8afab1c37b93b6a43c5489570005537099
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-06-15 22:43:30 +0200

    adapt logger to railway

--- src/logger.ts
+++ src/logger.ts
@@ -2,15 +2,20 @@ import pino from "pino";
 import { env } from "./env";
 
 export const logger = pino({
-	level: env.LOG_LEVEL,
-	...(env.NODE_ENV === "development"
-		? {
-				transport: {
-					target: "pino-pretty",
-					options: {
-						colorize: true,
-					},
-				},
-			}
-		: {}),
+	transport:
+		env.NODE_ENV === "development"
+			? { target: "pino-pretty", options: { colorize: true } }
+			: undefined,
+	customLevels: {
+		debug: 20,
+		info: 30,
+		warn: 40,
+		error: 50,
+	},
+	useOnlyCustomLevels: true,
+	formatters: {
+		level(label) {
+			return { level: label };
+		},
+	},
 });
