768494313163fdd2aa05b4fbeaa03e6b54860263
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-07-18 20:45:08 +0200

    add logs

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import { Database } from "bun:sqlite";
 // biome-ignore lint/nursery/noUndeclaredDependencies: <explanation>
 import { file } from "bun";
 import { drizzle } from "drizzle-orm/bun-sqlite";
+import { logger } from "../logger";
 // @ts-expect-error
 import migrationFile from "./_migration.sql" with { type: "file" };
 import { schema } from "./schema";
@@ -9,4 +10,11 @@ import { schema } from "./schema";
 const sqlite = new Database();
 sqlite.run(await file(migrationFile).text());
 
-export const db = drizzle(sqlite, { schema });
+export const db = drizzle(sqlite, {
+	schema,
+	logger: {
+		logQuery(query, params) {
+			logger.debug(params, query);
+		},
+	},
+});
