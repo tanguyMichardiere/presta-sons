397ea8cd59c49835bf2f51fef8d7c97fd6074161
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-07-18 22:53:06 +0200

    fix

--- src/db/schema/groupMembers.ts
+++ src/db/schema/groupMembers.ts
@@ -10,7 +10,7 @@ export const groupMembers = sqliteTable(
 		groupId: integer("group_id")
 			.references(() => groups.id, { onDelete: "cascade" })
 			.notNull(),
-		memberId: integer("member_ids")
+		memberId: integer("member_id")
 			.references(() => members.id, { onDelete: "cascade" })
 			.notNull(),
 	},
