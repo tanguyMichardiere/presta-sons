e42c3ccfad5d9da5fe6ce7618f54dc986a4b61f7
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2024-07-18 20:06:19 +0200

    remove log

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,6 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
-import { logger } from "../logger";
 // @ts-expect-error
 import sqlite from "./db.sqlite" with { type: "sqlite", embed: "true" };
 import { schema } from "./schema";
 
 export const db = drizzle(sqlite, { schema });
-
-logger.info(JSON.stringify(sqlite));
