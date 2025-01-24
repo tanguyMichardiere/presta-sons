3af65140183c8f7b199ad0f7d3ba82ca893ccd61
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2025-01-24 18:28:56 +0100

    update bun

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,12 +2,12 @@ import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { logger } from "../logger";
 // @ts-expect-error
-import migrationFile from "./_migration.sql" with { type: "file" };
+import migration from "./_migration.sql" with { type: "text" };
 import { schema } from "./schema";
 
 const sqlite = new Database();
 sqlite.run("PRAGMA foreign_keys = ON;");
-sqlite.run(await Bun.file(migrationFile).text());
+sqlite.run(migration);
 logger.debug("successfully applied the database migration");
 
 export const db = drizzle(sqlite, {
